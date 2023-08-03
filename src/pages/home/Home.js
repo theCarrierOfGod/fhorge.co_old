0e68eaa40cf70ff1b8e6445ba203e6a0a9fe0bdf
unknown <mrspecial932@gmail.com>
2023-08-02 22:36:47 -1100

    specia

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer'
 import Navigation from '../components/Navigation/Navigation'
+import Earn from './../Earn/Earn'
+import  Search  from './../search/search'
 
 const Home = () => {
   return (
     <>
       <Navigation />
+      <Earn/>
+      
 
       <Footer />
     </>
